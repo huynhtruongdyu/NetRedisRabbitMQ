f4510f4713c5a3484c3f7049e8ab543811de5087
Duy Huynh <huynhtruongdyu@gmail.com>
2023-10-10 17:42:46 +0700

    add syncronous methods on cache service

--- Services/Common/Service.Common/Abstracttion/Services/ICacheService.cs
+++ Services/Common/Service.Common/Abstracttion/Services/ICacheService.cs
@@ -11,5 +11,15 @@
         Task RemoveAsync(string cachKey, CancellationToken cancellationToken = default);
 
         Task RemoveByPrefixAsync(string prefixKey, CancellationToken cancellationToken = default);
+
+        T? Get<T>(string cacheKey);
+
+        void Set<T>(string cacheKey, T value);
+
+        bool SetIfNotExists<T>(string cacheKey, T value);
+
+        void Remove(string cachKey);
+
+        void RemoveByPrefix(string prefixKey);
     }
 }
--- Services/Common/Service.Common/Infrastructure/Services/CacheService.cs
+++ Services/Common/Service.Common/Infrastructure/Services/CacheService.cs
@@ -6,6 +6,7 @@ using Service.Common.Abstracttion.Services;
 
 using System.Collections.Concurrent;
 using System.Text;
+using System.Threading;
 
 namespace Service.Common.Infrastructure.Services
 {
@@ -19,6 +20,17 @@ namespace Service.Common.Infrastructure.Services
             _distributedCache = distributedCache;
         }
 
+        public T? Get<T>(string cacheKey)
+        {
+            byte[]? cacheValue = _distributedCache.Get(cacheKey);
+            if (cacheValue == null)
+            {
+                return default(T?);
+            }
+            T? value = JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(cacheValue));
+            return value;
+        }
+
         public async Task<T?> GetAsync<T>(string cacheKey, CancellationToken cancellationToken = default)
         {
             byte[]? cacheValue = await _distributedCache.GetAsync(cacheKey, cancellationToken);
@@ -30,10 +42,24 @@ namespace Service.Common.Infrastructure.Services
             return value;
         }
 
+        public void Remove(string cachKey)
+        {
+            _distributedCache.Remove(cachKey);
+            CacheKeys.TryRemove(cachKey, out bool _);
+        }
+
         public async Task RemoveAsync(string cachKey, CancellationToken cancellationToken = default)
         {
             await _distributedCache.RemoveAsync(cachKey, cancellationToken);
             CacheKeys.TryRemove(cachKey, out bool _);
+        }
+
+        public void RemoveByPrefix(string prefixKey)
+        {
+            foreach (var key in CacheKeys.Keys.Where(key => key.StartsWith(prefixKey)))
+            {
+                Remove(key);
+            }
         }
 
         public async Task RemoveByPrefixAsync(string prefixKey, CancellationToken cancellationToken = default)
@@ -42,11 +68,31 @@ namespace Service.Common.Infrastructure.Services
             await Task.WhenAll(removeAsyncTasks);
         }
 
+        public void Set<T>(string cacheKey, T value)
+        {
+            string cacheValue = JsonConvert.SerializeObject(value);
+            _distributedCache.Set(cacheKey, Encoding.UTF8.GetBytes(cacheValue));
+            CacheKeys.TryAdd(cacheKey, false);
+        }
+
         public async Task SetAsync<T>(string cacheKey, T value, CancellationToken cancellationToken = default)
         {
             string cacheValue = JsonConvert.SerializeObject(value);
             await _distributedCache.SetAsync(cacheKey, Encoding.UTF8.GetBytes(cacheValue), cancellationToken);
             CacheKeys.TryAdd(cacheKey, false);
+        }
+
+        public bool SetIfNotExists<T>(string cacheKey, T value)
+        {
+            var existsValue = _distributedCache.GetString(cacheKey);
+            if (existsValue != null)
+            {
+                return false;
+            }
+            string cacheValue = JsonConvert.SerializeObject(value);
+            _distributedCache.Set(cacheKey, Encoding.UTF8.GetBytes(cacheValue));
+            CacheKeys.TryAdd(cacheKey, false);
+            return true;
         }
 
         public async Task<bool> SetIfNotExistsAsync<T>(string cacheKey, T value, CancellationToken cancellationToken = default)
