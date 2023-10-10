7adf237dfdbfbe2502229bd4efa29c0855d070c5
Duy Huynh <huynhtruongdyu@gmail.com>
2023-10-11 01:43:25 +0700

    wip

--- Services/ServiceA/ServiceA.API/Controllers/OrdersController.cs
+++ Services/ServiceA/ServiceA.API/Controllers/OrdersController.cs
@@ -28,14 +28,12 @@ namespace ServiceA.API.Controllers
         [HttpPost("place-order")]
         public async Task<IActionResult> PlaceOrderAsync()
         {
-            if (!_cache.KeyExists("iphone13"))
+            if (!await _cache.KeyExistsAsync("iphone13"))
             {
-                _cache.SetIfNotExists<long>("iphone13", 0);
+                await _cache.SetIfNotExistsAsync<long>("iphone13", 0);
             }
 
-            var slBanra = _cache.Get<long>("iphone13");
-            Console.WriteLine("So luong truoc khi ban: " + slBanra);
-            slBanra = _cache.IncreaseBy("iphone13", 1);
+            var slBanra = await _cache.IncreaseByAsync("iphone13", 1);
 
             if (slBanra > 10)
             {
@@ -44,11 +42,6 @@ namespace ServiceA.API.Controllers
             }
 
             Console.WriteLine("So luong sau khi ban: " + slBanra);
-
-            if (slBanra > 10)
-            {
-                _cache.Set<bool>("banquaroi", true);
-            }
 
             //_eventBus.Publish<CreateOrderEvent>(new CreateOrderEvent());
             return Ok("order created");
